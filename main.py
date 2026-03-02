6ac1c62862c059d887f0ec940d6d83e232f1ce07
Decid3t0 <aufaniz8152@gmail.com>
2026-03-02 09:32:58 +0700

    second commit

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
+
+
 list_1 = ["aufa", 1, True, 3.14]
+list_1.append("aura")
+list_1.insert(3, "jowo borot")
 
 print(list_1[0])
 print(list_1[1])
 print(list_1[2])
 print(list_1[3])
+print(list_1[4])
+print(list_1[5])
+
+
+print("Hello World")
+
+tuple_1 = ("aufa", 1, True, 3.14)
+a, b, c, d, = tuple_1
+
+print(tuple_1[-4])
+
+print(a, b, c, d,)
+print(type(a), type(b), type(c), type(d))
